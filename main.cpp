ba679a1392cb8d5cdab9176b557229a13b589196
Legendary-Phoenix <shobbhan9124@gmail.com>
2025-06-24 15:07:41 +0800

    Some minor changes

--- main.cpp
+++ main.cpp
@@ -17,21 +17,20 @@ int main()
         return 1;
     }
     cout << "I reach here:" << endl;
-    string poo;
     string line;
-    //Read column headings and ignore it
+    // Read column headings and ignore it
     getline(file, line);
 
-    //Loop till end of file reading row by row
+    // Loop till end of file reading row by row
     int count = 0;
 
     while (getline(file, line) && count < 300)
     {
-        //Split value by comma
+        // Split value by comma
         stringstream ss(line);
-        //Store comma separated values in values array (there's 18 values per row)
+        // Store comma separated values in values array (there's 18 values per row)
         string values[18], token;
-        int poop = 0;
+        int i = 0;
         while (getline(ss, token, ',') && i < 18)
         {
             values[i++] = token;
