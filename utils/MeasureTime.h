cf93e2d203b68a53a3f18e67b7e805dac9ce5a3e
Muhammad Ridwan <157098120+Muhd-Ridwan@users.noreply.github.com>
2025-07-04 17:21:32 +0800

    Adding measure Time

--- utils/MeasureTime.h
+++ utils/MeasureTime.h
@@ -1,4 +1,4 @@
-#pragma once
+/*#pragma once
 
 #include <chrono>
 #include <iostream>
@@ -15,4 +15,27 @@ void measureAndReport(const string &label, const function<void()> &func)
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(end - start);
     cout << label << " completed in " << duration.count() << "ms\n";
+}*/
+
+#pragma once
+
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <functional>
+
+using namespace std;
+using namespace std::chrono;
+
+void measureAndReport(const string &label, const function<void()> &func)
+{
+    auto start = high_resolution_clock::now();
+    func();
+    auto end = high_resolution_clock::now();
+
+    auto durationMs = duration_cast<milliseconds>(end - start);
+    double durationMinutes = durationMs.count() / 60000.0; // 1 min = 60000 ms
+
+    cout << label << " completed in " << durationMs.count() << " ms ";
+    cout << "(" << fixed << setprecision(4) << durationMinutes << " minutes)" << endl;
 }
